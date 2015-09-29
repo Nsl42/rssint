9aa3f31d22ee5d9d9ddac146eac8d51c31504c26
Romain Bressan <romain@brss.me>
2015-09-29 22:40:11 +0200

    Phelps 0.2: read feeds file
    Read file in file given as argument. Each line in feeds file is a link to a RSS
    or Atom file. Parse each feed and print parsed data.

--- phelps.py
+++ phelps.py
@@ -1,9 +1,26 @@
-#!/bin/python
+#!/bin/python2
 
 import sys 
 import feedparser
-import argparse
+
+def spyder(url):
+    d = feedparser.parse(url)
+    print('title:\t%s' % d.feed.title)
+    print('link:\t%s' % d.feed.link)
+    print('desc:\t%s' % d.feed.description)
+    # print('date:\t%s' % d.feed.published)
+    print()
+    for l in d.entries:
+        print(' title:\t%s' % l.title)
+        print(' link:\t%s' % l.link)
+        print(' desc:\t%s' % l.description)
+        print(' date:\t%s' % l.published)
+        print(' id:\t%s' % l.id)
+    print()
+    print()
+
 
 for arg in sys.argv[1:]:
-	d = feedparser.parse (arg)
-	print(d['feed']['title'])
+    with open(arg) as f:
+        for line in f:
+            spyder(line)
