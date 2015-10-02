2df9c1c6de35e85a2a91556cd3a874b7f41d4611
nsl42 <nslmdrhr@gmail.com>
2015-10-02 18:44:24 +0200

    Ajout fonction writer et résolution pb d'encodage

--- phelps.py
+++ phelps.py
@@ -2,22 +2,33 @@
 
 import sys 
 import feedparser
+import hashlib
+
+ITEMLOC = '/tmp/rssint/'
+
+def writer(id, entry, feedlink):
+    with open(ITEMLOC + id, 'w') as f:
+        f.write('Title: %s\n' % entry.title.encode('utf-8'))
+        f.write('Date: %s\n' % entry.published)
+        f.write('Link: %s\n' % entry.link)
+        f.write('Feed: %s\n' % feedlink)
+        f.write('\n')
+        f.write(entry.description.encode('utf-8'))
+        f.write('\n')
 
 def spyder(url):
     d = feedparser.parse(url)
     print('title:\t%s' % d.feed.title)
     print('link:\t%s' % d.feed.link)
     print('desc:\t%s' % d.feed.description)
-    # print('date:\t%s' % d.feed.published)
-    print()
+    #print('date:\t%s' % d.feed.published)
+    print
     for l in d.entries:
-        print(' title:\t%s' % l.title)
-        print(' link:\t%s' % l.link)
-        print(' desc:\t%s' % l.description)
-        print(' date:\t%s' % l.published)
-        print(' id:\t%s' % l.id)
-    print()
-    print()
+        #Generating the Id : SHA1 over l.description
+        key = hashlib.sha256(l.description.encode('utf-8'))
+        writer(key.hexdigest(), l, d.feed.link)
+    print
+    print
 
 
 for arg in sys.argv[1:]:
